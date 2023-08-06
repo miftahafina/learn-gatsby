65bd63b454d50dd1008f8113afaba54725168453
Miftah Afina <miftah.afina@gmail.com>
2023-08-06 14:56:47 +0700

    Show featured image

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Layout from '../../components/Layout'
 import * as styles from '../../styles/projects.module.css'
 import { Link, graphql } from 'gatsby'
+import Img from "gatsby-image"
 
 export default function Projects({ data }) {
   console.log(data)
@@ -17,6 +18,7 @@ export default function Projects({ data }) {
           {projects.map(project => (
             <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
               <div>
+                <Img fluid={project.frontmatter.thumb.childImageSharp.fluid} />
                 <h3>{project.frontmatter.title}</h3>
                 <p>{project.frontmatter.stack}</p>
               </div>
@@ -38,11 +40,17 @@ export const query = graphql`
           title
           stack
           slug
+          thumb {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
         }
         id
       }
     }
-
     contact: site {
       siteMetadata {
         contact
